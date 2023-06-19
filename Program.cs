1270a6b7f928c216d89ec779f3c9e2ac34cd8676
Никита Богачев <nikitabogachev@rambler.ru>
2023-06-19 14:15:00 +0300

    Добавлен подсчет количества элементов в массиве, длина которых меньше или равна 3

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
 ﻿string[] createArrayManually(int size)
 {
-    string[] newArray = new string[size];
+    string[] array = new string[size];
     for (int i = 0; i < size; i++)
     {
         Console.WriteLine($"Введите {i + 1}й элемент массива: ");
-        newArray[i] = Console.ReadLine();
+        array[i] = Console.ReadLine();
     }
+    return array;
+}
+
+int CountLength(string[] array)
+{
+    int elementLength3 = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            elementLength3++;
+        }
+    }
+    return elementLength3;
+}
+
+string[] createFilterArray(string[] array, int elementLength3)
+{
+    string[] newArray = new string[elementLength3];
+    
     return newArray;
 }
 
@@ -22,3 +42,6 @@ Console.Write("Введите размерность массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
 string[] myArray = createArrayManually(size);
 printArray(myArray);
+int filter = CountLength(myArray);
+string[] myFilterArray = createFilterArray(myArray, filter);
+printArray(myFilterArray);
